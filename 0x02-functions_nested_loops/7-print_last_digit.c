ae4beb075021aef68e00e54abec4bfcfcdc3f6d1
krieglro <krieglro@gmail.com>
2022-09-14 14:34:26 -0700

    another change

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit -> prints last digit
+ * @n: argument passed
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+	a = n % 10;
+	_putchar(x + '0');
+	return (n % 10);
+}

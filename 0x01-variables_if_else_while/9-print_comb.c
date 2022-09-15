6d60485b4112310061a3f7dfd0cb034ae8e29b9b
krieglro <krieglro@gmail.com>
2022-09-15 09:41:51 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * main combinations of a number 
+/**
+ * main - combinations of a number
+ * @void: shows main doesn't take any arguement
+ *
+ * Description: the function prints a combination of a number
+ * Return: Always 0
  */
-
 int main(void)
-{ 
-	
+{
 	int n = 48;
+
 	while (n <= 1020)
 	{
 		putchar(n);
 		if (n == 57)
-			n += 39
+			n += 39;
 		++n;
 	}
-
 	putchar('\n');
-
-	return(0);
+	return (0);
 }

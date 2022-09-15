49876cc3b7a312b2dc0365d5ea8228af91d2b49a
krieglro <krieglro@gmail.com>
2022-09-15 08:51:53 -0700

    new

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- *main prints base 10 numbers 
+/**
+ * main - prints base 10 numbers
+ * @void: shows main doesnt take any arguements
+ *
+ * Description: prints base 10 numbers
+ * Return: Always 0
  */
 
 int main(void)
 {
 	int n = 0;
-	while (n <= 9){
+
+	while (n <= 9)
+	{
 		putchar(n + '0');
 		++n;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }

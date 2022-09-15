62adc05099700b4198b7d8b58d7236054f830ab3
krieglro <krieglro@gmail.com>
2022-09-15 09:09:09 -0700

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * main prints reverse alphabet
+/**
+ * main - prints reverse alphabet
+ * @void: shows main doesn't take argument
+ *
+ * Description: prints the alphabet fomr z-aa
+ * Return: Always 0
  */
 
 int main(void)
-{ 
+{
 	char lower = 'z';
-	while(lower >= 'a'){
+
+	while (lower >= 'a')
+	{
 		putchar(lower);
 		--lower;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }

7cc1718d5d6d4ead8e1dcc27584450d76d2abdb3
krieglro <krieglro@gmail.com>
2022-09-15 09:20:12 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * main prints all base 16 numbers in lower case
+/**
+ * main - prints all base 16 numbers in lower case
+ * @void: shows that main doesn't take any arguments
+ *
+ * Description: prints all base 16 numbers in lower case
+ * Return: Always 0
  */
 
 int main(void)
 {
 	int n = 48;
-	while (n <= 102) 
+
+	while (n <= 102)
 	{
 		putchar(n);
 		if (n == 57)
@@ -17,5 +22,5 @@ int main(void)
 	}
 
 	putchar('\n');
-	return(0);
+	return (0);
 }

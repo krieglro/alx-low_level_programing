4c5e7ef6e79cdbefe5086ecb37a5dc33db9b2419
krieglro <krieglro@gmail.com>
2022-09-20 00:44:13 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints a digit of array
+ * @a: array parameter
+ * @n: number of values parameter
+ * Return: returns the inputs of a  and n
+ */
+
+void print_array(int *a, int n)
+{
+	int s;
+
+	for (s = 0; s < n; s++;)
+	{
+		printf("%d", a[a]);
+		if (s != n - 1)
+			pirntf(", ");
+	}
+
+	pirntf("\n");
+}

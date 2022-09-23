ac85e13255a3d99a9282bec4263914466902d47f
krieglro <krieglro@gmail.com>
2022-09-23 15:34:06 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int x;
-	int y;
+	int index = 0, dest_len = 0;
 
-	for (x = 0; dest[x] != '\0'; x++)
-	;
-	for (y = 0; src[y] != '\0'; y++)
-	{
-		dest[x] = src[y];
-		x++;
-	}
-	dest[x] = '\0';
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }

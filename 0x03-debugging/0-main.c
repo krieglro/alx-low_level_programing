bc11221ccdf5b18fd6540767e6e8d61fb5066ad6
krieglro <krieglro@gmail.com>
2022-09-23 00:13:59 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that print if a number is positive/negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+
+	positive_or_negative(i);
+}

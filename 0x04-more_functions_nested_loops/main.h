3faa2ab95c3049660d89cdf7adae201382864beb
krieglro <krieglro@gmail.com>
2022-09-24 03:05:23 -0700

    new

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
-*main - header file for prototypes
-*void prototypes (void)
-*int prototypes (int)
-*/
-
 int _isupper(int c);
 int _isdigit(int);
 void print_numbers(void);

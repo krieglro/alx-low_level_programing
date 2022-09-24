83715c561f9bc5faf47eb22727647eb394b07916
krieglro <krieglro@gmail.com>
2022-09-24 03:59:41 -0700

    new

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,10 +3,10 @@
 /**
 *_isdigit - checks is character is a digit
 *@c: tests character
-*Return: 1 or 0
+*Return: success 
 */
 
 int _isdigit(int c)
 {
-	return (c <= '0' && c >= '9');
+	return (c >= '0' && c <= '9');
 }

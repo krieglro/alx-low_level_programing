fe5c7a41468169e18e03661f16f5384129da1d6a
krieglro <krieglro@gmail.com>
2022-09-25 13:21:08 -0700

    new files

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,6 +11,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int inddex = 0, dest_len = 0;
+
+	while (dest[idex++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }

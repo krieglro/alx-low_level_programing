fb242b1b5ff7785ba632ae734e87ddefeee2125a
krieglro <krieglro@gmail.com>
2022-09-25 13:50:47 -0700

    new files

--- 0x06-pointers_arrays_strings/2_strncpy.c
+++ 0x06-pointers_arrays_strings/2_strncpy.c
@@ -11,6 +11,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,24 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int i = 0, diff = 0;
+
+	while (1)
 	{
-		s1++;
-		s2++;
+		if (s1[i] == '\0' && s2[i] == '\0')
+			break;
+		else if (s1[i] == '\0')
+		{
+			diff = s2[i];
+			break;
+		}
+		else if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			break;
+		}
+		else
+			i++;
 	}
 
-	return (*s1 - *s2)
 }

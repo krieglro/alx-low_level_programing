e453c1150ada797b0e9de6a6fe25bd7b9d7811bd
krieglro <krieglro@gmail.com>
2022-09-25 14:28:49 -0700

    new files

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 /**
  * _strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
- * @dest: string that will be appended
+ * @dest: String that will be appended
  * @src: String to be concatenated upon
  *
  * Return: returns pointer to @dest
@@ -16,7 +16,7 @@ char *_strcat(char *dest, char *src)
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[inex]; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);

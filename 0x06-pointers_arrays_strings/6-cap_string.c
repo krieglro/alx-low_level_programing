0613da98c0bc8cf1669ce8110ba2354f81bac77e
krieglro <krieglro@gmail.com>
2022-09-25 15:17:30 -0700

    new files

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *s)
 	int i = 0, j;
 	char a[] = "\t\n,;.!?\"(){}";
 
-	while (*(s + i) >= 'a' && *(s + i) <= 'z')
+	while (*(s + i))
 	{
 		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{

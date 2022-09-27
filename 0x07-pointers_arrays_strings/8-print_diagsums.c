d59a729f24f4a5751763198b3e46f61d8bad77c5
krieglro <krieglro@gmail.com>
2022-09-27 02:10:00 -0700

    new stuff

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -23,8 +23,8 @@ void print_diagsums(int *a, int size)
 		}
 		if (i % (size - 1) == 0 && (i > 0 && (i < (size * size - 1))))
 		{
-			sum2 += a[i];
+			s2 += a[i];
 		}
 	}
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", s1, s2);
 }

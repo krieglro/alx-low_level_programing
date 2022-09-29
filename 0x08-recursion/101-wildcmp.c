76e8ad5540589958d6a43f5efda79fe3cd079cb2
krieglro <krieglro@gmail.com>
2022-09-29 00:14:00 -0700

    new change

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * wildcmp - this function compares two strings
+ * @s1: string to be checked
+ * @s2: the pattern to be used
+ *
+ * Return: returns 1 if identical and 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '\0')
+		return (*s1 == '\0');
+	if (*s2 == *s1)
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}

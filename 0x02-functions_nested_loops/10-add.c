95472dd210bcbc5bcfb45365d07132396a3ec001
krieglro <krieglro@gmail.com>
2022-09-29 10:41:50 -0700

    new

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ * @num1: the first integer parameter
+ * @num2: the second integer parameter
+ *
+ * Return: returns the result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}

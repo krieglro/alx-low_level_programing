3c884dfd308e01803f99eac0c1cf4fc4f76cebe6
krieglro <krieglro@gmail.com>
2022-09-30 04:04:24 -0700

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - this is a function to print its name
+ * main - function that prints its name
  * @argc: argc parameter
- * @argv: an array of a command listed
+ * @argv: an array of command listed
  * Return: 0 for success
  */
 int main(int argc __attribute__((unused)), char *argv[])

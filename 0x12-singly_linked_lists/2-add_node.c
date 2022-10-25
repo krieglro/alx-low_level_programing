454b58bb2dc0b9852c83369a17b578cc142be791
krieglro <krieglro@gmail.com>
2022-10-25 05:00:03 -0700

    new

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	size_t nchar;
 
-	new = malloc(sizef(list_t));
+	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);

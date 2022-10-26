0e5ffcc993d5efcff6415fc3713d5c881a3d053c
krieglro <krieglro@gmail.com>
2022-10-26 03:11:11 -0700

    new

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nnodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nnodes++;
+
 		printf("%d\n", h->n);
 		h = h->next;
+		nnodes++;
 	}
 	return (nnodes);
 }

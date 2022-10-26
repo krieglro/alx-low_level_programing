acabb5065f150f40bf16291eb9a822ad5d219874
krieglro <krieglro@gmail.com>
2022-10-26 01:57:15 -0700

    new

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,19 +4,18 @@
  * print_listint - prints all the elements of a list.
  * @h: head of a list
  *
- * Return: the numbers of nodes
+ * Return: numbers of nodes
  */
 
-size_t print_listint(const listint_t *h)
+size_t print_list(const listint_t *h)
 {
-	const listint_t *cursor = h;
-	size_t count = 0;
+	size_t nnodes = 0;
 
-	while (cursor != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", cursor->n);
-		count += 1;
-		cursor = cursor->next;
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
 	}
-	return (count);
+	return (nnodes);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,8 +9,7 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node
- * structure
+ * Description: singly linked list node structure
  * for Holberton project
  */
 

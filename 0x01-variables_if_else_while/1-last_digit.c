50dfe006bafc4766bd284e79a32dda325eaa2725
krieglro <krieglro@gmail.com>
2022-11-23 15:21:34 -0800

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,23 +5,22 @@
 /**
  * main - prints a random number
  *
- * Return:  Always 0
+ * Return: Always returns 0
  */
-
 int main(void)
 {
+	int n;
 
-	char zh = 'a';
+	int m;
 
-	while (zh <= 'z')
-	{
-		putchar (zh);
-
-	++zh;
-
-	}
-	putchar ('\n');
-
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	m = n % 10;
+	if (m > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, m);
+	if (m == 0)
+		printf("Last digit of %d is %d and is 0\n", n, m);
+	if (m < 6 && m != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
-
 }

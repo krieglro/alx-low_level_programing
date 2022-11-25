549057f6d62cc9ee56135d4dbc9a737a7cff0500
krieglro <krieglro@gmail.com>
2022-11-25 01:13:18 -0800

    new

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - prints
- *
- * Return: 0 on success
- */
-
 int main(void)
 {
 	int n;
@@ -14,7 +8,17 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	*(p + 5) = 98;
-	printf("a[2] = %d\n", a[2]);
+/*
+ * write your line of code here...
+ * Remember:
+ * main - magic
+ * - you are not allowed to use a
+ *   - you are not allowed to modify p
+ *   - only one statement
+ *   - you are not aloowed to code anything else than this line of code
+ */
+	*(p + 5) - 98;
+/* ...so that this prints 98\n */
+	printf("a[z] = %d\n", a[z]);
 	return (0);
 }

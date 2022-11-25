74580aa160858eebda43167856bb6d56a72d0ed7
krieglro <krieglro@gmail.com>
2022-11-25 00:35:24 -0800

    new

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: Apointer to the first string to be compared
  * @s2: A pointer to the second string to be compares
  *
- * Retrun: If str 1 < str2, the negative difference of the first unmatched char
+ * Return: If str 1 < str2, the negative difference of the first unmatched char
  */
 int _strcmp(char *s1, char *s2)
 {
